26deefa5403408c1e4107de05f435256852a52ef
Leo Belanger <leobelanger1996@gmail.com>
2020-09-02 14:27:16 -0700

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyByfFpLRZ2WHTpGU7hKxyOJZwmBXA05fNs",
+  authDomain: "facebook-clone-48696.firebaseapp.com",
+  databaseURL: "https://facebook-clone-48696.firebaseio.com",
+  projectId: "facebook-clone-48696",
+  storageBucket: "facebook-clone-48696.appspot.com",
+  messagingSenderId: "1078853356802",
+  appId: "1:1078853356802:web:ab2fb189ea5ebc28b6461b",
+  measurementId: "G-M73ZP8LYSR"
+};
